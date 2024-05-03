fbd8a788287e6e6ce80fcdd93b8f0663bde68683
ZiadMohamed123 <153671040+ZiadMohamed123@users.noreply.github.com>
2024-05-03 23:19:58 +0300

    Payment form

---
+++ Plans/Plans-JavaScript.js
@@ -0,0 +1,10 @@
+function openpayment() {
+    window.open('Payment.Index.html', '_self');
+  }
+
+  document.getElementById('subscribebtn').addEventListener('click', openpayment);
+  function openpayment2() {
+    window.open('Payment.Index.html', '_self');
+  }
+
+  document.getElementById('subscribebtn2').addEventListener('click', openpayment);
