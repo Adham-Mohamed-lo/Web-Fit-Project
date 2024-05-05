3600421db06578777d73b523e42d64b425630d18
ZiadMohamed123 <153671040+ZiadMohamed123@users.noreply.github.com>
2024-05-06 01:40:35 +0300

    premium

--- Plans/Plans-JavaScript.js
+++ Plans/Plans-JavaScript.js
@@ -1,10 +1,13 @@
 function openpayment() {
     window.open('Payment.Index.html', '_self');
   }
-
   document.getElementById('subscribebtn').addEventListener('click', openpayment);
+
   function openpayment2() {
     window.open('Payment.Index.html', '_self');
   }
-
-  document.getElementById('subscribebtn2').addEventListener('click', openpayment);
+  document.getElementById('subscribebtn2').addEventListener('click', openpayment2);
+  function opencoaches() {
+    window.open('../Services/coaches-page.html', '_self');
+  }
+  document.getElementById('subscribebtn3').addEventListener('click', opencoaches);
